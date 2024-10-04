686989bcd5e3c2579c2e810d9678dd2a6d1fa02c
Kharitonov Pavel <p.kharitonov@cn-innov.ru>
2024-10-04 20:07:19 +0300

    initial commit

---
+++ plaminskiy-research/src/test/java/org/plaminsky/research/PlaminskiyResearchApplicationTests.java
@@ -0,0 +1,13 @@
+package org.plaminsky.research;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlaminskiyResearchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
